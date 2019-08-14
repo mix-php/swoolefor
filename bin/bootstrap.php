b4b0c664b7d23484098b58d8f71372cf55c3dec5
Jian Liu <onanying@126.com>
2019-08-14 12:08:33 +0800

    入口文件判断swoole

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -6,10 +6,12 @@ require __DIR__ . '/../vendor/autoload.php';
 use Mix\Concurrent\Coroutine;
 
 // Coroutine
-Coroutine::enableHook(SWOOLE_HOOK_ALL ^ SWOOLE_HOOK_FILE);
-Coroutine::set([
-    'max_coroutine' => 300000,
-]);
+if (extension_loaded('swoole')) {
+    Coroutine::enableHook(SWOOLE_HOOK_ALL ^ SWOOLE_HOOK_FILE);
+    Coroutine::set([
+        'max_coroutine' => 300000,
+    ]);
+}
 
 // Run application
 $config = require __DIR__ . '/../config/main.php';
